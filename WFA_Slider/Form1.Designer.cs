7c1e552521687bd5cd8a98e94a35ff7c5e5ef562
akifharmandar <akifharmandar@hotmail.com>
2019-08-10 18:46:13 +0300

    slider.project

---
+++ WFA_Slider/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WFA_Slider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.ImageList = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(767, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton1.Location = new System.Drawing.Point(23, 366);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(107, 23);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "|<<";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.MetroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton2.Location = new System.Drawing.Point(136, 366);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(114, 23);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "<<";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.MetroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton3.Location = new System.Drawing.Point(256, 366);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(300, 23);
+            this.metroButton3.TabIndex = 1;
+            this.metroButton3.Text = "START-STOP";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.MetroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton4.Location = new System.Drawing.Point(562, 366);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(114, 23);
+            this.metroButton4.TabIndex = 2;
+            this.metroButton4.Text = ">>";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.MetroButton4_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton5.Location = new System.Drawing.Point(682, 366);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(107, 23);
+            this.metroButton5.TabIndex = 3;
+            this.metroButton5.Text = ">>|";
+            this.metroButton5.UseSelectable = true;
+            this.metroButton5.Click += new System.EventHandler(this.MetroButton5_Click);
+            // 
+            // ImageList
+            // 
+            this.ImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImageList.ImageStream")));
+            this.ImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImageList.Images.SetKeyName(0, "5d17b6402af10706d0241f75 (1).jpg");
+            this.ImageList.Images.SetKeyName(1, "481166.jpg");
+            this.ImageList.Images.SetKeyName(2, "c-sharp-visual-studio-logo-1280x720.jpg");
+            this.ImageList.Images.SetKeyName(3, "IMG_5091.bmp");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // metroTrackBar1
+            // 
+            this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar1.Location = new System.Drawing.Point(23, 394);
+            this.metroTrackBar1.Maximum = 10000;
+            this.metroTrackBar1.Minimum = 100;
+            this.metroTrackBar1.Name = "metroTrackBar1";
+            this.metroTrackBar1.Size = new System.Drawing.Size(766, 23);
+            this.metroTrackBar1.TabIndex = 4;
+            this.metroTrackBar1.Text = "metroTrackBar1";
+            this.metroTrackBar1.Value = 100;
+            this.metroTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.MetroTrackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.metroTrackBar1);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private System.Windows.Forms.ImageList ImageList;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WFA_Slider/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+namespace WFA_Slider
+{
+    public partial class Form1 : MetroForm
+
+    {
+        int indexImageRnd = 0;
+        int indexImage = 0;
+        bool startsActivated = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroButton3_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+        Random rnd = new Random();
+  
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            indexImageRnd = rnd.Next(0, ImageList.Images.Count);
+            pictureBox1.Image = ImageList.Images[indexImageRnd];
+            startsActivated = false;
+        }
+
+        private void MetroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            label1.Text = metroTrackBar1.Value.ToString();
+            timer1.Interval = metroTrackBar1.Value;
+        }
+
+        private void MetroButton1_Click(object sender, EventArgs e)
+        {
+            indexImage = 0;
+            pictureBox1.Image = ImageList.Images[indexImage];
+        }
+
+        private void MetroButton5_Click(object sender, EventArgs e)
+        {
+            indexImage = ImageList.Images.Count - 1;
+            pictureBox1.Image = ImageList.Images[indexImage];
+            
+        }
+
+        private void MetroButton2_Click(object sender, EventArgs e)
+        {
+            if (startsActivated == false)
+            {
+                for (int i = 0; i < ImageList.Images.Count; i++)
+                {
+                    if (pictureBox1.Image == ImageList.Images[i])
+                    {
+                        indexImage = i;
+                    }
+                }
+            }
+            else
+                indexImage = indexImageRnd;
+            if (indexImage>0)
+            {
+                indexImage -= 1;
+                pictureBox1.Image = ImageList.Images[indexImage];
+                indexImageRnd = indexImage;
+            }
+            else
+            {
+                pictureBox1.Image = ImageList.Images[indexImage];
+            }
+
+        }
+
+        private void MetroButton4_Click(object sender, EventArgs e)
+        {
+           
+            if (startsActivated == false)
+            {
+                for (int i = 0; i < ImageList.Images.Count; i++)
+                {
+                    if (pictureBox1.Image == ImageList.Images[i])
+                    {
+                        indexImage = i;
+                    }
+                }
+            }
+            else
+                indexImage = indexImageRnd;
+            if (indexImage < ImageList.Images.Count-1)
+            {
+                indexImage += 1;
+                pictureBox1.Image = ImageList.Images[indexImage];
+                indexImageRnd = indexImage;
+            }
+            else
+            {
+                pictureBox1.Image = ImageList.Images[indexImage];
+            }
+        }
+    }
+}
